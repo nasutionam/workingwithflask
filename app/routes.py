f0d4c897d9d15e50d36769ad6196bcc3807e4eb8
Muhammad Asri Nasution <nasution.nam@gmail.com>
2020-07-19 12:06:59 +0700

    templates & forms

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,29 @@
 from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Nam'}
-    return '''
-        <html>
-            <head>
-                <title>Home Page</title>
-            </head>
-            <body>
-                <h1>Hello ''' + user['username'] + '''</h1>
-            </body>
-        </html>
-    '''
+    posts = [
+        {
+            'author': {'username':'Jhon'},
+            'body': 'Beautiful day in indonesia'
+        },
+        {
+            'author': {'username':'Susan'},
+            'body':'The avengers movie was so cool'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user{}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
